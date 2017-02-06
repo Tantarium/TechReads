29e3b2fbe546933807144ad56b4da03db911eb28
LarryPohto <LPohto@manifestcorp.com>
2017-02-06 15:50:28 -0500

    Downloaded Spring Boot and imported it into Eclipse.

---
+++ src/main/java/com/manifesrtcorp/TechreadsApplication.java
@@ -0,0 +1,12 @@
+package com.manifesrtcorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechreadsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/manifesrtcorp/TechreadsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.manifesrtcorp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechreadsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
